ebddbf9300a0ebecd291a99b80b72b5e917b62c0
javierRodriguezUPC <120645986+javierRodriguezUPC@users.noreply.github.com>
2023-05-22 11:57:11 +0200

    newIssue api post works

--- src/views/NewIssueView.js
+++ src/views/NewIssueView.js
@@ -3,55 +3,70 @@ import { useEffect, useState } from 'react';
 
 
 export const NewIssueView = () => {
+    const apiUrl = 'https://issuetracker2-asw.herokuapp.com/issues/';
 
-    const [subject, setSubject] = useState();
-    const [description, setDescription] = useState();
 
+    const [subject, setSubject] = useState('');
+    const [description, setDescription] = useState('');
+    const [apiResponse, setApiResponse] = useState('');
+
+    const handleSubjectChange = (event) => {
+        setSubject(event.target.value);
+    };
+
+    const handleDescriptionChange = (event) => {
+        setDescription(event.target.value);
+    };
+
+
+    const handleClick = async () => {
+        try {    
+          const headers = {
+            Authorization: 'Token a571977cf3bf557efd80fb12cd154fb6b46aa307',
+            'Content-Type': 'application/json'
+          };
     
-    const submit = () => {
-        setSubject()
-        setDescription()
-    }
+          const data = {
+            Subject: subject,
+            Description: description
+          };
+    
+          const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify(data)
+          });
+    
+          const responseData = await response.json();
+          setApiResponse(responseData)
+    
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      };
 
   return (
         <div>
             <h2 class="title">New Issue</h2>
             <div>
                 <div>
-                    <input type="text" name="Subject" placeholder="Subject" value={subject} required data-lenght="500"/>
+                    <input type="text" name="Subject" placeholder="Subject" value={subject} required data-lenght="500" onChange={handleSubjectChange}/>
                 </div>
                 <div>
-                    <textarea rows="7" name="Description" placeholder="Enter a description" value={description}></textarea>
+                    <textarea rows="7" name="Description" placeholder="Enter a description" value={description} onChange={handleDescriptionChange}></textarea>
                 </div>
-                <button onClick={submit}>Create</button>
+                <button onClick={handleClick}>Create</button>
+                <div>
+                    {apiResponse && (
+                    <div>
+                        <p>ID: {apiResponse.id}</p>
+                        <p>Subject: {apiResponse.Subject}</p>
+                        <p>Description: {apiResponse.Description}</p>
+                        {/* Muestra las propiedades del objeto apiResponse */}
+                    </div>
+                    )}
+                </div>
             </div>
         </div>
   )
 }
-
-
-/*
-<body>
-    <div class = "lightbox">
-        <form class = "lightbox-form" method="POST" action="https://issuetracker2-asw.herokuapp.com/issues">
-        <h2 class="title">New Issue</h2>
-        <div>
-            <div class="form-wrapper">
-            <div class="main">
-                <fieldset>
-                <input type="text" name="Subject" placeholder="Subject" required data-lenght="500"/>
-                </fieldset>
-                <fieldset>
-                <textarea rows="7" name="Description" placeholder="Enter a description"></textarea>
-                </fieldset>
-            </div>
-            </div>
-            <div class="btn-container">
-            <input type="submit" name="create" class="create action-button" value="Create"/>
-            </div>
-        </div>
-        </form>
-    </div>
-    <button class="close-button" onclick="location.href='{{ base_url }}'">X</button>
-    </body>
-*/

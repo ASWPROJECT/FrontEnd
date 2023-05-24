ae7c3f27074cd2ab2ee940f0eb14a6bc581f26b1
santilara <santilaracelis16@gmail.com>
2023-05-24 18:43:27 +0200

    ADD users visualization

--- src/views/IssuesView.js
+++ src/views/IssuesView.js
@@ -5,7 +5,9 @@ import '../assets/css/IssuesViewStyles.css'
 
 export const IssuesView = () => {
   const apiUrl = 'https://issuetracker2-asw.herokuapp.com/issues/';
+  const apiUsersUrl = 'https://issuetracker2-asw.herokuapp.com/users/users/';
   const [apiResponse, setApiResponse] = useState('');
+  const [usersList, setUsersList] = useState('');
   const [searchQuery, setSearchQuery] = useState('');
   const [status, setStatus] = useState('');
   const [priority, setPriority] = useState('');
@@ -29,12 +31,28 @@ export const IssuesView = () => {
       } catch (error) {
         console.error('Error:', error);
       }
+
+      try {
+        const headers = {
+          Authorization: 'Token a571977cf3bf557efd80fb12cd154fb6b46aa307',
+          'Content-Type': 'application/json'
+        };
+
+        const response = await fetch(apiUsersUrl, {
+          method: 'GET',
+          headers,
+        });
+
+        const responseData = await response.json();
+        setUsersList(responseData);
+      } catch (error) {
+        console.error('Error:', error);
+      }
     };
 
     fetchData();
   }, []); // La dependencia vacía [] asegura que el efecto solo se ejecute una vez al montar el componente
 
-  console.log(apiResponse);
 
   const deleteIssue = async (issueId) => {
     try {    
@@ -69,8 +87,8 @@ export const IssuesView = () => {
     if (priority) params.append('priority', priority);
     if (creator) params.append('creator', creator);
     if (orderBy) params.append('order_by', orderBy);
+
     const urlWithParams = `${apiUrl}?${params.toString()}`;
-    console.log(urlWithParams)
 
     // Realizar la petición con la URL construida
     try {
@@ -114,6 +132,14 @@ export const IssuesView = () => {
 
   const handleOrderByChange = (event) => {
     setOrderBy(event.target.value);
+  };
+
+  const handleResetSelects = () => {
+    setSearchQuery("");
+    setStatus("");
+    setPriority("");
+    setCreator("");
+    setOrderBy("");
   };
 
   return (
@@ -161,8 +187,13 @@ export const IssuesView = () => {
             </label>
             <select className="select-filters" id="creator" name="creator" value={creator} onChange={handleCreatorChange}>
               <option value=""></option>
-              {/* PARA CADA USUARIO */}
-              <option value="USERNAME"></option>
+              {Array.isArray(usersList) ? (
+                usersList.map(user => (
+                  <option value={user.user_username}>{user.user_username}</option>
+                ))
+              ) : (
+                null
+              )}
             </select>
 
             <label className="search-bar" htmlFor="order_by">
@@ -179,7 +210,7 @@ export const IssuesView = () => {
             <button className="search-action-button" onClick={searchIssues} title="Filter the selected params"></button>
           </div>
           <div class="filter-form" method="GET" action={ apiUrl }>
-            <button type="submit" class="clear-filters-button" title="Clear filters"></button>
+            <button onClick={handleResetSelects} class="clear-filters-button" title="Clear filters"></button>
           </div>  
         </div>
         <div class="new-issue">

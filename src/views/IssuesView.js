b66d9d4e50d635806376e80e54a753a970c05d26
santilara <santilaracelis16@gmail.com>
2023-05-24 17:06:20 +0200

    ADD FILTERS VIEWS

--- src/views/IssuesView.js
+++ src/views/IssuesView.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Issue } from '../components/Issue.js';
 import { NavigationBar } from '../components/NavigationBar.js';
+import '../assets/css/IssuesViewStyles.css'
 
 export const IssuesView = () => {
   const apiUrl = 'https://issuetracker2-asw.herokuapp.com/issues/';
@@ -55,9 +56,61 @@ export const IssuesView = () => {
   };
 
   return (
-    <div>
+    <div class="WNDW">
       <NavigationBar></NavigationBar>
+      <div class="taskboard-actions">
+        <div class="issue-table-options">
+          <div class="filter-form" >
+            <input class="search-bar" type="text" name="q" placeholder="subject or description" autocomplete="on" ></input>
 
+            <label class="search-bar" for="status">Status:</label>
+            <select class="select-filters" id="status" name="status">                
+                <option value="" ></option>
+                <option value="New">New</option>
+                <option value="In progress">In progress</option>
+                <option value="Ready for test">Ready for test</option>
+                <option value="Closed">Closed</option>
+                <option value="Needs info">Needs info</option>
+                <option value="Rejected">Rejected</option>
+                <option value="Postponed">Postponed</option>
+            </select>
+
+            <label class="search-bar" for="priority">Priority:</label>
+            <select class="select-filters" id="priority" name="priority">
+                <option value="" ></option>
+                <option value="Low" >Low</option>
+                <option value="Normal" >Normal</option>
+                <option value="High">High</option>
+            </select>
+
+            <label class="search-bar" for="creator">Created by:</label>
+            <select class="select-filters" id="creator" name="creator">
+              <option value="" ></option>
+              {/* PARA CADA USUARIO */}
+              <option value="USERNAME"></option>
+            </select>
+
+            <label class="search-bar" for="order_by">Order by:</label>
+            <select class="select-filters" id="order_by" name="order_by">                
+                <option value=""></option>
+                <option value="Subject" >Subject ascending</option>
+                <option value="-Subject">Subject descending</option>
+                <option value="Created_at">Created at ascending</option>
+                <option value="-Created_at">Created at descending</option>
+            </select>
+          
+            <button type="submit" class="search-action-button" title="Filter the selected params"></button>
+          </div>
+          <form class="filter-form" method="GET" action={ apiUrl }>
+            <button type="submit" class="clear-filters-button" title="Clear filters"></button>
+          </form>  
+        </div>
+        <div class="new-issue">
+          <button class="new-issue-btn" onclick="window.location.href='/newIssues/'">NEW ISSUE</button>        
+          <button class="bulk-insert-btn" onclick="window.location.href='/bulk_insert/'"></button>  
+        </div>
+
+      </div> 
       {Array.isArray(apiResponse) ? (
         apiResponse.map(issue => (
           <Issue id={issue.id} subject={issue.Subject} Description={issue.Description} Blocked={issue.Block_reason} onDelete={deleteIssue}/>
